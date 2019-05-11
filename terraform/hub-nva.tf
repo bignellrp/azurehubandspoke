006cbf403b7bff287fa7cba2e9407c34994296c0
Richard Bignell <bignellrp@users.noreply.github.com>
2019-05-11 19:46:35 +0100

    Modified for additional NVA with multiple interfaces

--- terraform/hub-nva.tf
+++ terraform/hub-nva.tf
@@ -1,6 +1,6 @@
 locals {
   prefix-hub-nva         = "hub-nva"
-  hub-nva-location       = "CentralUS"
+  hub-nva-location       = "uksouth"
   hub-nva-resource-group = "hub-nva-rg"
 }
 
@@ -13,8 +13,8 @@ resource "azurerm_resource_group" "hub-nva-rg" {
   }
 }
 
-resource "azurerm_network_interface" "hub-nva-nic" {
-  name                 = "${local.prefix-hub-nva}-nic"
+resource "azurerm_network_interface" "hub-nva1-nic1" {
+  name                 = "${local.prefix-hub-nva}-nic1"
   location             = "${azurerm_resource_group.hub-nva-rg.location}"
   resource_group_name  = "${azurerm_resource_group.hub-nva-rg.name}"
   enable_ip_forwarding = true
@@ -23,7 +23,7 @@ resource "azurerm_network_interface" "hub-nva-nic" {
     name                          = "${local.prefix-hub-nva}"
     subnet_id                     = "${azurerm_subnet.hub-dmz.id}"
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.0.36"
+    private_ip_address            = "10.0.0.36" #Needs changing
   }
 
   tags {
@@ -31,11 +31,67 @@ resource "azurerm_network_interface" "hub-nva-nic" {
   }
 }
 
-resource "azurerm_virtual_machine" "hub-nva-vm" {
+resource "azurerm_network_interface" "hub-nva1-nic2" {
+  name                 = "${local.prefix-hub-nva}-nic2"
+  location             = "${azurerm_resource_group.hub-nva-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-nva-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub-nva}"
+    subnet_id                     = "${azurerm_subnet.hub-dmz.id}" #Need to add a private subnet for second nic
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.37" #Needs changing
+  }
+
+  tags {
+    environment = "${local.prefix-hub-nva}"
+  }
+}
+
+resource "azurerm_network_interface" "hub-nva2-nic1" {
+  name                 = "${local.prefix-hub-nva}-nic1"
+  location             = "${azurerm_resource_group.hub-nva-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-nva-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub-nva}"
+    subnet_id                     = "${azurerm_subnet.hub-dmz.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.38" #Needs changing
+  }
+
+  tags {
+    environment = "${local.prefix-hub-nva}"
+  }
+}
+
+resource "azurerm_network_interface" "hub-nva2-nic2" {
+  name                 = "${local.prefix-hub-nva}-nic2"
+  location             = "${azurerm_resource_group.hub-nva-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-nva-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub-nva}"
+    subnet_id                     = "${azurerm_subnet.hub-dmz.id}" #Need to add a private subnet for second nic
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.39" #Needs changing
+  }
+
+  tags {
+    environment = "${local.prefix-hub-nva}"
+  }
+}
+
+# Create NVA1
+
+resource "azurerm_virtual_machine" "hub-nva1-vm" {
   name                  = "${local.prefix-hub-nva}-vm"
   location              = "${azurerm_resource_group.hub-nva-rg.location}"
   resource_group_name   = "${azurerm_resource_group.hub-nva-rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.hub-nva-nic.id}"]
+  network_interface_ids = ["${azurerm_network_interface.hub-nva1-nic1.id}" "${azurerm_network_interface.hub-nva1-nic2.id}"]
   vm_size               = "${var.vmsize}"
 
   storage_image_reference {
@@ -67,6 +123,46 @@ resource "azurerm_virtual_machine" "hub-nva-vm" {
   }
 }
 
+# Create NVA2 - Duplicated from above for second nva
+
+resource "azurerm_virtual_machine" "hub-nva2-vm" {
+  name                  = "${local.prefix-hub-nva}-vm"
+  location              = "${azurerm_resource_group.hub-nva-rg.location}"
+  resource_group_name   = "${azurerm_resource_group.hub-nva-rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.hub-nva2-nic1.id}" "${azurerm_network_interface.hub-nva2-nic2.id}"]
+  vm_size               = "${var.vmsize}"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${local.prefix-hub-nva}-vm"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "${local.prefix-hub-nva}"
+  }
+}
+
+# May need to modify this script below to apply the Cisco config
+
 resource "azurerm_virtual_machine_extension" "enable-routes" {
   name                 = "enable-iptables-routes"
   location             = "${azurerm_resource_group.hub-nva-rg.location}"
@@ -90,6 +186,8 @@ SETTINGS
   }
 }
 
+# Create hub RT
+
 resource "azurerm_route_table" "hub-gateway-rt" {
   name                          = "hub-gateway-rt"
   location                      = "${azurerm_resource_group.hub-nva-rg.location}"
@@ -102,30 +200,20 @@ resource "azurerm_route_table" "hub-gateway-rt" {
     next_hop_type  = "VnetLocal"
   }
 
-  route {
-    name                   = "toSpoke1"
-    address_prefix         = "10.1.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.0.0.36"
-  }
-
-  route {
-    name                   = "toSpoke2"
-    address_prefix         = "10.2.0.0/16"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = "10.0.0.36"
-  }
-
   tags {
     environment = "${local.prefix-hub-nva}"
   }
 }
+
+# Assoc Hub RT to subnet
 
 resource "azurerm_subnet_route_table_association" "hub-gateway-rt-hub-vnet-gateway-subnet" {
   subnet_id      = "${azurerm_subnet.hub-gateway-subnet.id}"
   route_table_id = "${azurerm_route_table.hub-gateway-rt.id}"
   depends_on = ["azurerm_subnet.hub-gateway-subnet"]
 }
+
+# Create Spoke RT - Shouldnt this be part of the Spoke TF and in the spoke RG
 
 resource "azurerm_route_table" "spoke1-rt" {
   name                          = "spoke1-rt"
@@ -134,8 +222,8 @@ resource "azurerm_route_table" "spoke1-rt" {
   disable_bgp_route_propagation = false
 
   route {
-    name                   = "toSpoke2"
-    address_prefix         = "10.2.0.0/16"
+    name                   = "toHub"
+    address_prefix         = "10.0.0.0/8"
     next_hop_type          = "VirtualAppliance"
     next_hop_in_ip_address = "10.0.0.36"
   }
@@ -170,8 +258,8 @@ resource "azurerm_route_table" "spoke2-rt" {
   disable_bgp_route_propagation = false
 
   route {
-    name                   = "toSpoke1"
-    address_prefix         = "10.1.0.0/16"
+    name                   = "toHub"
+    address_prefix         = "10.0.0.0/8"
     next_hop_in_ip_address = "10.0.0.36"
     next_hop_type          = "VirtualAppliance"
   }
