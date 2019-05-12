6fab398406db9647aff7c66b77f07f0dbf19a690
Richard Bignell <bignellrp@users.noreply.github.com>
2019-05-12 19:44:19 +0100

    Update hub-vnet.tf
    Added LB Config

--- terraform/hub-vnet.tf
+++ terraform/hub-vnet.tf
@@ -1,9 +1,9 @@
-# Set local variables (check globals in variables file)
+# Define local variables (check globals in parameters file)
 
 locals {
-  prefix-hub         = "hub"
-  hub-location       = "uksouth"
-  hub-resource-group = "hub-vnet-rg"
+  prefix-hub            = "hub"
+  hub-location          = "uksouth"
+  hub-resource-group    = "hub-vnet-rg"
 }
 
 # Create Hub Resource Group
@@ -42,4 +42,60 @@ resource "azurerm_subnet" "hub-public" {
   resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
   virtual_network_name = "${azurerm_virtual_network.hub-vnet.name}"
   address_prefix       = "10.74.9.0/25"
+}
+
+# Create LB
+
+resource "azurerm_lb" "azlb" {
+  name                = "${local.prefix-hub}-lb"
+  location            = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name = "${azurerm_resource_group.hub-vnet-rg.name}"
+
+  frontend_ip_configuration {
+    name                          = "${local.prefix-hub}-fip"
+    subnet_id                     = "${azurerm_subnet.hub-private.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.74.9.134"
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create AddressPool - Not sure how to add addresses
+
+resource "azurerm_lb_backend_address_pool" "azlb" {
+  resource_group_name = "${azurerm_resource_group.hub-vnet-rg.name}"
+  loadbalancer_id     = "${azurerm_lb.azlb.id}"
+  name                = "BackEndAddressPool"
+}
+
+# Create LB Probe
+
+resource "azurerm_lb_probe" "azlb" {
+  resource_group_name = "${azurerm_resource_group.hub-vnet-rg.name}"
+  loadbalancer_id     = "${azurerm_lb.azlb.id}"
+  name                = "${local.prefix-hub}-probe"
+  protocol            = "tcp"
+  port                = "22"
+  interval_in_seconds = "5"
+  number_of_probes    = "2"
+}
+
+Create LB Rule
+
+resource "azurerm_lb_rule" "azlb" {
+  resource_group_name            = "${azurerm_resource_group.hub-vnet-rg.name}"
+  loadbalancer_id                = "${azurerm_lb.azlb.id}"
+  name                           = "${local.prefix-hub}-rule"
+  protocol                       = "All"
+  frontend_port                  = "All"
+  backend_port                   = "All"
+  frontend_ip_configuration_name = "${local.prefix-hub}-fip"
+  enable_floating_ip             = false
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.azlb.id}"
+  idle_timeout_in_minutes        = 5
+  probe_id                       = "${azurerm_lb_probe.azlb.id}"
+  depends_on                     = ["azurerm_lb_probe.azlb"]
 }
