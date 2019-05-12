f435026d1301de2e1db7223c27fe1a4ed2dcdd15
Richard Bignell <bignellrp@users.noreply.github.com>
2019-05-12 08:10:35 +0100

    Update on-prem.tf
    Just added some comments and added second rtr vm

--- terraform/on-prem.tf
+++ terraform/on-prem.tf
@@ -1,13 +1,19 @@
+# Set local variables (check globals in variables file)
+
 locals {
   onprem-location       = "ukwest"
   onprem-resource-group = "onprem-vnet-rg"
   prefix-onprem         = "onprem"
 }
 
+# Create Onprem Resource Group
+
 resource "azurerm_resource_group" "onprem-vnet-rg" {
   name     = "${local.onprem-resource-group}"
   location = "${local.onprem-location}"
 }
+
+# Create Onprem VNET and set CIDR
 
 resource "azurerm_virtual_network" "onprem-vnet" {
   name                = "onprem-vnet"
@@ -20,12 +26,16 @@ resource "azurerm_virtual_network" "onprem-vnet" {
   }
 }
 
+# Create Onprem Public Subnet
+
 resource "azurerm_subnet" "onprem-public" {
   name                 = "public"
   resource_group_name  = "${azurerm_resource_group.onprem-vnet-rg.name}"
   virtual_network_name = "${azurerm_virtual_network.onprem-vnet.name}"
   address_prefix       = "10.0.0.0/24"
 }
+
+# Create Onprem Private Subnet
 
 resource "azurerm_subnet" "onprem-private" {
   name                 = "private"
@@ -34,8 +44,10 @@ resource "azurerm_subnet" "onprem-private" {
   address_prefix       = "10.0.1.0/24"
 }
 
-resource "azurerm_public_ip" "onprem-pip" {
-    name                = "${local.prefix-onprem}-pip"
+# Create PublicIP for VM
+
+resource "azurerm_public_ip" "onprem-pip1" {
+    name                = "${local.prefix-onprem}-pip1"
     location            = "${azurerm_resource_group.onprem-vnet-rg.location}"
     resource_group_name = "${azurerm_resource_group.onprem-vnet-rg.name}"
     allocation_method   = "Dynamic"
@@ -44,6 +56,21 @@ resource "azurerm_public_ip" "onprem-pip" {
         environment = "${local.prefix-onprem}"
     }
 }
+
+# Create PublicIP for Onprem Router VM
+
+resource "azurerm_public_ip" "onprem-pip2" {
+    name                = "${local.prefix-onprem}-pip2"
+    location            = "${azurerm_resource_group.onprem-vnet-rg.location}"
+    resource_group_name = "${azurerm_resource_group.onprem-vnet-rg.name}"
+    allocation_method   = "Dynamic" # Need to change this to static
+
+    tags {
+        environment = "${local.prefix-onprem}"
+    }
+}
+
+# Create NIC for VM
 
 resource "azurerm_network_interface" "onprem-nic" {
   name                 = "${local.prefix-onprem}-nic"
@@ -55,11 +82,43 @@ resource "azurerm_network_interface" "onprem-nic" {
     name                          = "${local.prefix-onprem}"
     subnet_id                     = "${azurerm_subnet.onprem-private.id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.onprem-pip.id}"
+    public_ip_address_id          = "${azurerm_public_ip.onprem-pip1.id}"
+  }
+}
+
+# Create NIC1 for Router VM
+
+resource "azurerm_network_interface" "onprem-rtr-nic1" {
+  name                 = "${local.prefix-onprem}-rtr-nic1"
+  location             = "${azurerm_resource_group.onprem-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.onprem-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-onprem}"
+    subnet_id                     = "${azurerm_subnet.onprem-public.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.onprem-pip2.id}"
+  }
+}
+
+# Create NIC2 for Router VM
+
+resource "azurerm_network_interface" "onprem-rtr-nic2" {
+  name                 = "${local.prefix-onprem}-rtr-nic2"
+  location             = "${azurerm_resource_group.onprem-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.onprem-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-onprem}"
+    subnet_id                     = "${azurerm_subnet.onprem-private.id}"
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Create Network Security Group and rule
+
 resource "azurerm_network_security_group" "onprem-nsg" {
     name                = "${local.prefix-onprem}-nsg"
     location            = "${azurerm_resource_group.onprem-vnet-rg.location}"
@@ -82,16 +141,58 @@ resource "azurerm_network_security_group" "onprem-nsg" {
     }
 }
 
-resource "azurerm_subnet_network_security_group_association" "mgmt-nsg-association" {
+# Associate NSG with Private Subnet
+
+resource "azurerm_subnet_network_security_group_association" "private-nsg-association" {
   subnet_id                 = "${azurerm_subnet.onprem-private.id}"
   network_security_group_id = "${azurerm_network_security_group.onprem-nsg.id}"
 }
+
+# Create Onprem VM
 
 resource "azurerm_virtual_machine" "onprem-vm" {
   name                  = "${local.prefix-onprem}-vm"
   location              = "${azurerm_resource_group.onprem-vnet-rg.location}"
   resource_group_name   = "${azurerm_resource_group.onprem-vnet-rg.name}"
-  network_interface_ids = ["${azurerm_network_interface.onprem-nic.id}"] # Need to add an additional NIC
+  network_interface_ids = ["${azurerm_network_interface.onprem-nic.id}"]
+  vm_size               = "${var.vmsize}"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${local.prefix-onprem}-vm"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "${local.prefix-onprem}"
+  }
+}
+
+# Create Onprem Router VM
+
+resource "azurerm_virtual_machine" "onprem-rtr-vm" {
+  name                  = "${local.prefix-onprem}-rtr-vm"
+  location              = "${azurerm_resource_group.onprem-vnet-rg.location}"
+  resource_group_name   = "${azurerm_resource_group.onprem-vnet-rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.onprem-rtr-nic1.id}", "${azurerm_network_interface.onprem-rtr-nic2.id}"]
   vm_size               = "${var.vmsize}"
 
   storage_image_reference {
