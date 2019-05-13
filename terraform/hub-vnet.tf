01bad5ff2b6982affcb6a7a3405eb9af317ce867
Richard Bignell <bignellrp@users.noreply.github.com>
2019-05-13 19:30:34 +0100

    Merged NVA and Hub
    Merged NVA and Hub and change vm type to cisco

--- terraform/hub-vnet.tf
+++ terraform/hub-vnet.tf
@@ -44,6 +44,187 @@ resource "azurerm_subnet" "hub-public" {
   address_prefix       = "10.74.9.0/25"
 }
 
+# Create NIC1 for NVA1
+
+resource "azurerm_network_interface" "hub-nva1-nic1" {
+  name                 = "${local.prefix-hub}-nic1"
+  location             = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub}"
+    subnet_id                     = "${azurerm_subnet.hub-public.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.74.9.4"
+    primary                       = "true"
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create NIC2 for NVA1
+
+resource "azurerm_network_interface" "hub-nva1-nic2" {
+  name                 = "${local.prefix-hub}-nic2"
+  location             = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub}"
+    subnet_id                     = "${azurerm_subnet.hub-private.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.74.9.132"
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create NIC1 for NVA2
+
+resource "azurerm_network_interface" "hub-nva2-nic1" {
+  name                 = "${local.prefix-hub}-nic1"
+  location             = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub}"
+    subnet_id                     = "${azurerm_subnet.hub-public.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.74.9.5"
+    primary                       = "true"
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create NIC2 for NVA2
+
+resource "azurerm_network_interface" "hub-nva2-nic2" {
+  name                 = "${local.prefix-hub}-nic2"
+  location             = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "${local.prefix-hub}"
+    subnet_id                     = "${azurerm_subnet.hub-private.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.74.9.133"
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create NVA1 and attach NICS
+
+resource "azurerm_virtual_machine" "hub-nva1-vm" {
+  name                  = "${local.prefix-hub}-vm"
+  location              = "${azurerm_resource_group.hub-vnet-rg.location}"
+  resource_group_name   = "${azurerm_resource_group.hub-vnet-rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.hub-nva1-nic1.id}", "${azurerm_network_interface.hub-nva1-nic2.id}"]
+  primary_network_interface_id = "${azurerm_network_interface.hub-nva1-nic1.id}"
+  vm_size               = "${var.vmsize}"
+
+  storage_image_reference {
+    publisher = "Cisco"
+    offer     = "Cisco-CSR-1000V"
+    sku       = "csr-azure-byol"
+    version   = "3.16.10"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${local.prefix-hub}-vm"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "${local.prefix-hub}"
+  }
+}
+
+# Create NVA2 and attach NICS
+
+#resource "azurerm_virtual_machine" "hub-nva2-vm" {
+#  name                  = "${local.prefix-hub}-vm"
+#  location              = "${azurerm_resource_group.hub-vnet-rg.location}"
+#  resource_group_name   = "${azurerm_resource_group.hub-vnet-rg.name}"
+#  network_interface_ids = ["${azurerm_network_interface.hub-nva2-nic1.id}"]  # Needs a second interface
+#  vm_size               = "${var.vmsize}"
+#
+#  storage_image_reference {
+#    publisher = "Canonical"
+#    offer     = "UbuntuServer"
+#    sku       = "16.04-LTS"
+#    version   = "latest"
+#  }
+
+#  storage_os_disk {
+#    name              = "myosdisk2"
+#    caching           = "ReadWrite"
+#    create_option     = "FromImage"
+#    managed_disk_type = "Standard_LRS"
+#  }
+
+#  os_profile {
+#    computer_name  = "${local.prefix-hub}-vm"
+#    admin_username = "${var.username}"
+#    admin_password = "${var.password}"
+#  }
+
+#  os_profile_linux_config {
+#    disable_password_authentication = false
+#  }
+
+#  tags {
+#    environment = "${local.prefix-hub}"
+#  }
+#}
+
+# Can this be modified to support applying the Cisco config or should a bootstrap be used?
+
+#resource "azurerm_virtual_machine_extension" "enable-routes" {
+#  name                 = "enable-iptables-routes"
+#  location             = "${azurerm_resource_group.hub-vnet-rg.location}"
+#  resource_group_name  = "${azurerm_resource_group.hub-vnet-rg.name}"
+#  virtual_machine_name = "${azurerm_virtual_machine.hub-nva-vm.name}"
+#  publisher            = "Microsoft.Azure.Extensions"
+#  type                 = "CustomScript"
+#  type_handler_version = "2.0"
+#
+#  settings = <<SETTINGS
+#    {
+#        "commandToExecute": "bash enable-ip-forwarding.sh"
+#    }
+#SETTINGS
+#
+#  tags {
+#    environment = "${local.prefix-hub}"
+#  }
+#}
+
 # Create LB
 
 resource "azurerm_lb" "azlb" {
@@ -89,9 +270,9 @@ resource "azurerm_lb_rule" "azlb" {
   resource_group_name            = "${azurerm_resource_group.hub-vnet-rg.name}"
   loadbalancer_id                = "${azurerm_lb.azlb.id}"
   name                           = "${local.prefix-hub}-rule"
-  protocol                       = "All"
-  frontend_port                  = "0"
-  backend_port                   = "0"
+  protocol                       = "tcp" #All
+  frontend_port                  = "80" #0
+  backend_port                   = "80" #0
   frontend_ip_configuration_name = "${local.prefix-hub}-fip"
   enable_floating_ip             = false
   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.azlb.id}"
